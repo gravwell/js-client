ef4fdee8551b64109a8a61215f56bb782e4929a0
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-01-12 10:28:56 -0300

    Isolate playbook types

---
+++ src/models/playbook/index.ts
@@ -0,0 +1,13 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+export * from './is-playbook';
+export * from './playbook';
+export * from './raw-playbook';
+export * from './raw-playbook-decoded-metadata';
+export * from './to-playbook';
---
+++ src/models/playbook/raw-playbook-decoded-metadata.ts
@@ -0,0 +1,18 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { RawUUID } from '../../value-objects';
+
+export interface RawPlaybookDecodedMetadata {
+	dashboards: [];
+	attachments?: Array<{
+		fileGUID: RawUUID;
+		context: 'cover';
+		type: 'image';
+	}>;
+}
