0d0531ee6207df751307a6baa35ea4c297d85e70
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-01-12 17:25:31 -0300

    Move updatable saved query types to /models

--- src/functions/saved-queries/update-one-saved-query.spec.ts
+++ src/functions/saved-queries/update-one-saved-query.spec.ts
@@ -6,13 +6,13 @@
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
 
-import { CreatableSavedQuery, isSavedQuery, SavedQuery } from '../../models';
+import { CreatableSavedQuery, isSavedQuery, SavedQuery, UpdatableSavedQuery } from '../../models';
 import { integrationTest, myCustomMatchers } from '../../tests';
 import { TEST_AUTH_TOKEN, TEST_HOST } from '../../tests/config';
 import { makeCreateOneSavedQuery } from './create-one-saved-query';
 import { makeDeleteOneSavedQuery } from './delete-one-saved-query';
 import { makeGetAllSavedQueries } from './get-all-saved-queries';
-import { makeUpdateOneSavedQuery, UpdatableSavedQuery } from './update-one-saved-query';
+import { makeUpdateOneSavedQuery } from './update-one-saved-query';
 
 describe('updateOneSavedQuery()', () => {
 	const createOneSavedQuery = makeCreateOneSavedQuery({ host: TEST_HOST, useEncryption: false });
---
+++ src/models/saved-query/to-raw-updatable-saved-query.ts
@@ -0,0 +1,39 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { isNull, isUndefined } from 'lodash';
+import { toRawNumericID } from '../../value-objects';
+import { toRawTimeframe } from '../timeframe';
+import { RawUpdatableSavedQuery } from './raw-updatable-saved-query';
+import { SavedQuery } from './saved-query';
+import { UpdatableSavedQuery } from './updatable-saved-query';
+
+export const toRawUpdatableSavedQuery = (
+	updatable: UpdatableSavedQuery,
+	current: SavedQuery,
+): RawUpdatableSavedQuery => {
+	const defaultTimeframe = isUndefined(updatable.defaultTimeframe)
+		? current.defaultTimeframe
+		: updatable.defaultTimeframe;
+	const rawTimeframe = isNull(defaultTimeframe) ? null : toRawTimeframe(defaultTimeframe);
+
+	return {
+		ThingUUID: current.id,
+		GUID: updatable.globalID ?? current.globalID,
+
+		GIDs: (updatable.groupIDs ?? current.groupIDs).map(toRawNumericID),
+		Global: updatable.isGlobal ?? current.isGlobal,
+
+		Name: updatable.name ?? current.name,
+		Description: (isUndefined(updatable.description) ? current.description : updatable.description) ?? '',
+		Labels: updatable.labels ?? current.labels,
+
+		Query: updatable.query ?? current.query,
+		Metadata: { timeframe: rawTimeframe },
+	};
+};
