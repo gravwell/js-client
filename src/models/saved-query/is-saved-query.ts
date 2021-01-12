c3f12e5f31614441dadd1045ac05112ed89b0a5b
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-01-12 15:15:51 -0300

    Isolate saved query types

---
+++ src/models/saved-query/is-saved-query.ts
@@ -0,0 +1,32 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { isBoolean, isNull, isString } from 'lodash';
+import { isNumericID, isUUID } from '../../value-objects';
+import { isTimeframe } from '../timeframe';
+import { SavedQuery } from './saved-query';
+
+export const isSavedQuery = (value: any): value is SavedQuery => {
+	try {
+		const q = <SavedQuery>value;
+		return (
+			isUUID(q.id) &&
+			isUUID(q.globalID) &&
+			isNumericID(q.userID) &&
+			q.groupIDs.every(isNumericID) &&
+			isBoolean(q.isGlobal) &&
+			isString(q.name) &&
+			(isString(q.description) || isNull(q.description)) &&
+			q.labels.every(isString) &&
+			isString(q.query) &&
+			(isTimeframe(q.defaultTimeframe) || isNull(q.defaultTimeframe))
+		);
+	} catch {
+		return false;
+	}
+};
---
+++ src/models/saved-query/to-saved-query.ts
@@ -0,0 +1,34 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { isNull } from 'lodash';
+import { toNumericID } from '../../value-objects';
+import { toTimeframe } from '../timeframe';
+import { RawSavedQuery } from './raw-saved-query';
+import { SavedQuery } from './saved-query';
+
+export const toSavedQuery = (raw: RawSavedQuery): SavedQuery => {
+	const rawTimeframe = raw.Metadata?.timeframe ?? null;
+	const defaultTimeframe = isNull(rawTimeframe) ? null : toTimeframe(rawTimeframe);
+
+	return {
+		id: raw.ThingUUID,
+		globalID: raw.GUID,
+
+		userID: toNumericID(raw.UID),
+		groupIDs: raw.GIDs?.map(toNumericID) ?? [],
+		isGlobal: raw.Global ?? false,
+
+		name: raw.Name,
+		description: raw.Description.trim() === '' ? null : raw.Description,
+		labels: raw.Labels ?? [],
+
+		query: raw.Query,
+		defaultTimeframe,
+	};
+};
