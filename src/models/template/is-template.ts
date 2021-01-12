ee3b92842339f7916a49674f3a0adc0f895ef12e
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-01-12 19:37:48 -0300

    Isolate template types

---
+++ src/models/template/is-template.ts
@@ -0,0 +1,36 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { isBoolean, isDate, isNull, isString } from 'lodash';
+import { isNumericID, isUUID } from '../../value-objects';
+import { Template } from './template';
+
+export const isTemplate = (value: any): value is Template => {
+	try {
+		const t = <Template>value;
+		return (
+			isUUID(t.uuid) &&
+			isUUID(t.thingUUID) &&
+			isNumericID(t.userID) &&
+			t.groupIDs.every(isNumericID) &&
+			isString(t.name) &&
+			(isString(t.description) || isNull(t.description)) &&
+			t.labels.every(isString) &&
+			isBoolean(t.isGlobal) &&
+			isBoolean(t.isRequired) &&
+			isDate(t.lastUpdateDate) &&
+			isString(t.query) &&
+			isString(t.variable.token) &&
+			isString(t.variable.name) &&
+			(isString(t.variable.description) || isNull(t.variable.description)) &&
+			(isString(t.previewValue) || isNull(t.previewValue))
+		);
+	} catch {
+		return false;
+	}
+};
---
+++ src/models/template/to-template.ts
@@ -0,0 +1,36 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { RawTemplate } from './raw-template';
+import { Template } from './template';
+
+export const toTemplate = (raw: RawTemplate): Template => ({
+	uuid: raw.GUID,
+	thingUUID: raw.ThingUUID,
+
+	userID: raw.UID.toString(),
+	groupIDs: raw.GIDs?.map(id => id.toString()) ?? [],
+
+	name: raw.Name,
+	description: raw.Description.trim() === '' ? null : raw.Description,
+	labels: raw.Labels ?? [],
+
+	isGlobal: raw.Global,
+	isRequired: raw.Contents.required,
+
+	lastUpdateDate: new Date(raw.Updated),
+
+	query: raw.Contents.query,
+	variable: {
+		token: raw.Contents.variable,
+		name: raw.Contents.variableLabel,
+		description: raw.Contents.variableDescription?.trim() === '' ? null : raw.Contents.variableDescription,
+	},
+
+	previewValue: (raw.Contents.testValue ?? '').trim() === '' ? null : raw.Contents.testValue,
+});
