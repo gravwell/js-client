614a0bd66cdd35822ef32618442cebe2e13eb88d
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-01-12 19:54:20 -0300

    Isolate version types

---
+++ src/models/version/is-version.ts
@@ -0,0 +1,19 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+import { isInteger } from 'lodash';
+import { Version } from './version';
+
+export const isVersion = (value: any): value is Version => {
+	try {
+		const v = <Version>value;
+		return isInteger(v.major) && isInteger(v.minor) && isInteger(v.patch);
+	} catch {
+		return false;
+	}
+};
---
+++ src/models/version/raw-version.ts
@@ -0,0 +1,17 @@
+/*************************************************************************
+ * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
+/**
+ * @examples
+ * ```ts
+ * '3.5.7'
+ * '3'
+ * '1.5'
+ * ```
+ */
+export type RawVersion = string;
