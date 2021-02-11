d67ad5dcc5ab55a1088d38b0f18eda6f6ff692e1
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-02-11 18:09:31 -0300

    Update eslint rules

--- .config/.eslintrc.js
+++ .config/.eslintrc.js
@@ -13,15 +13,19 @@ module.exports = {
 	parserOptions: {
 		ecmaVersion: 2020,
 		sourceType: 'module',
+		project: [
+			'./.config/tsconfig.browsers-build.json',
+			'./.config/tsconfig.browsers-spec.json',
+			'./.config/tsconfig.node-build.json',
+			'./.config/tsconfig.node-spec.json',
+		],
 	},
 	plugins: ['@typescript-eslint', 'simple-import-sort', 'import'],
 	rules: {
 		'no-extra-semi': 'off',
 		'curly': 'off',
 		'no-mixed-spaces-and-tabs': 'off',
-
 		'no-unused-vars': 'off',
-		'@typescript-eslint/no-unused-vars': 'error',
 
 		'sort-imports': 'off',
 		'simple-import-sort/imports': ['error', { groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']] }],
@@ -29,19 +33,22 @@ module.exports = {
 		'import/newline-after-import': 'error',
 		'import/no-duplicates': 'error',
 
-		'@typescript-eslint/array-type': ['error', { default: 'generic' }],
-
+		'@typescript-eslint/ban-types': 'off',
 		'@typescript-eslint/explicit-function-return-type': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 		'@typescript-eslint/consistent-type-assertions': 'off',
 		'@typescript-eslint/no-use-before-define': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+
+		'@typescript-eslint/no-unused-vars': 'error',
+		'@typescript-eslint/array-type': ['error', { default: 'generic' }],
 	},
 	overrides: [
 		{
 			files: ['**/*.spec.ts'],
 			rules: {
-				'@typescript-eslint/no-use-before-define': 'off',
 				'@typescript-eslint/no-empty-function': 'off',
+				'@typescript-eslint/no-unused-vars': 'off',
 				'@typescript-eslint/no-non-null-assertion': 'off',
 			},
 		},
