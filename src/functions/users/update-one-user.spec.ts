7ae601cd3869a6ae102dc310333e226e52dd8114
Olivier Moratin <olivier.moratin@gravwell.io>
2021-08-09 15:07:19 -0700

    added search group id update to user update

--- src/functions/users/update-one-user.spec.ts
+++ src/functions/users/update-one-user.spec.ts
@@ -1,5 +1,5 @@
 /*************************************************************************
- * Copyright 2020 Gravwell, Inc. All rights reserved.
+ * Copyright 2021 Gravwell, Inc. All rights reserved.
  * Contact: <legal@gravwell.io>
  *
  * This software may be modified and distributed under the terms of the
@@ -173,4 +173,17 @@ describe('updateOneUser()', () => {
 			await expectAsync(login(user.username, newPassword)).toBeResolved();
 		}),
 	);
+
+	xit(
+		'Should update the user search group ID',
+		integrationTest(async () => {
+			expect(isValidUser(user)).toBeTrue();
+
+			// Update the password
+			await expectAsync(updateOneUser({ id: user.id, searchGroupID : '1' })).toBeResolved();
+
+		}),
+	);
+
+
 });
