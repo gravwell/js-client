bd99164e54513aaf168c909d93f8d8764c42f8bd
Olivier Moratin <olivier.moratin@gravwell.io>
2021-08-13 17:30:01 -0700

    added license

--- src/functions/mail-server/conversion.ts
+++ src/functions/mail-server/conversion.ts
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * Copyright 2021 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
 import {MailServerConfig, RawMailServerConfig, MailServerTestData, RawMailServerTestData} from '../../models/mail-server';
 import {isString} from 'lodash';
 
--- src/models/mail-server/is-mail-server-config.ts
+++ src/models/mail-server/is-mail-server-config.ts
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * Copyright 2021 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
 import {MailServerConfig} from './mail-server-config';
 import {isPlainObject, isBoolean, isString, isNumber, isNil} from 'lodash';
 
