7b8fda7d4d6b924ddc84b080db5b93f7cc39afbc
Guilherme Puszkarek <52247963+Puszkarek@users.noreply.github.com>
2021-09-17 10:09:27 -0300

    fix/239#SearchSubscription should NOT request data instantly

---
+++ src/functions/utils/interval-handler.ts
@@ -0,0 +1,41 @@
+import { timer } from 'rxjs';
+
+//Types
+interface IntervalHandlerProps {
+	intervalStepSize: number;
+	intervalOffset: number;
+	intervalInitialValue: number;
+}
+
+// Implementations
+export const initIntervalHandler = ({
+	intervalStepSize,
+	intervalOffset,
+	intervalInitialValue,
+}: IntervalHandlerProps) => {
+	let interval = intervalInitialValue - intervalStepSize;
+	const getIntervalTime = () => {
+		if (interval < intervalOffset) interval += intervalStepSize;
+		return interval;
+	};
+	const resetInterval = () => {
+		interval = intervalInitialValue;
+	};
+	return { getIntervalTime, resetInterval };
+};
+
+export const initDynamicDelay = ({ intervalStepSize, intervalOffset, intervalInitialValue }: IntervalHandlerProps) => {
+	const { getIntervalTime, resetInterval } = initIntervalHandler({
+		intervalStepSize,
+		intervalOffset,
+		intervalInitialValue,
+	});
+
+	//return delay observable
+	const dynamicDelay = <T>(event: any) => {
+		if (event?.finished) resetInterval();
+		const delayTime = getIntervalTime();
+		return timer(delayTime);
+	};
+	return { dynamicDelay, resetInterval };
+};
