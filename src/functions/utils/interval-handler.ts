1e2c0d55fe9c5c7ed0de2373f7d9f46aeff93fff
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-09-22 12:06:37 -0300

    Format

--- src/functions/utils/interval-handler.ts
+++ src/functions/utils/interval-handler.ts
@@ -10,16 +10,14 @@ import { timer } from 'rxjs';
 
 /**
  * Props to configure the delay
- *
- * @param 	stepSizeValue every interval that finished !== true, add this value to the timer
- * @param 	offsetValue the timer should not pass that limit
- * @param 	initialValue initial time value to start delay
  */
 export interface DelayHandlerProps {
 	/** every interval that finished !== true, add this value to the timer  */
 	stepSizeValue: number;
+
 	/** the timer should not pass that limit */
 	offsetValue: number;
+
 	/** initial time value to start delay */
 	initialValue: number;
 }
@@ -27,21 +25,19 @@ export interface DelayHandlerProps {
 /**
  * Initialize timer handler and returns a function
  * that manages the timer and return its value
- *
- *
- * @param 	stepSizeValue - Every interval that finished !== true, add this value to the timer
- * @param 	offsetValue the timer should not pass that limit
- * @param 	initialValue initial timer value
  */
 export const initIntervalHandler = ({ stepSizeValue, offsetValue, initialValue }: DelayHandlerProps) => {
 	let interval = initialValue - stepSizeValue;
+
 	const getIntervalTime = () => {
 		if (interval < offsetValue) interval += stepSizeValue;
 		return interval;
 	};
+
 	const resetInterval = () => {
 		interval = initialValue - stepSizeValue;
 	};
+
 	return { getIntervalTime, resetInterval };
 };
 
@@ -63,7 +59,7 @@ export const initIntervalHandler = ({ stepSizeValue, offsetValue, initialValue }
 export const initDynamicDelay = ({ stepSizeValue, offsetValue, initialValue }: DelayHandlerProps) => {
 	const { getIntervalTime, resetInterval } = initIntervalHandler({ stepSizeValue, offsetValue, initialValue });
 
-	//return delay observable
+	// return delay observable
 	const dynamicDelay = <T>(value: T) => {
 		if (typeof value === 'object') {
 			const data = (value as unknown) as object & { finished?: boolean };
