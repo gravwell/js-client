4329ed85d612e59a6e8540254d7de83493443efa
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-09-22 18:25:41 -0300

    Delete unnecessary files

--- src/functions/utils/retry-async/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './retry-async';
-export * from './retry-async-lazy';
-export * from './retry-async-options';
--- src/functions/utils/wait.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns an empty (void) promise that resolves after {time} milliseconds.
- *
- * @param time Time in milliseconds to wait until resolving the promise
- * @return {Promise<void>} Promise resolving after time
- */
-export const wait = (time: number): Promise<void> => new Promise<void>(resolve => setTimeout(resolve, time));
