1d7caec90980c97891f0869f6227adfc3146ecb0
Michael Wisely <michael.wisely@gravwell.io>
2021-09-28 09:45:49 -0600

    Simplify body of rxjsDynamicDuration

--- src/functions/utils/rxjs-dynamic-duration.ts
+++ src/functions/utils/rxjs-dynamic-duration.ts
@@ -6,9 +6,9 @@
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
 
+import { isNull } from 'lodash';
 import { Observable, timer } from 'rxjs';
 import { first, mapTo } from 'rxjs/operators';
-import { createMappableValue } from './create-mappable-value';
 
 /**
  * Returns a function that receives a value and returns that value wrapped in an observable that
@@ -44,12 +44,14 @@ export const rxjsDynamicDuration = <T>(
 	mapDuration: (lastDuration: number, event: T, index: number) => number,
 	initialDuration = 0,
 ) => {
-	const mappableDuration = createMappableValue(mapDuration, initialDuration);
+	let index = 0;
+	let previousDuration: number | null = null;
 
 	// Return duration observable
 	return (value: T): Observable<T> => {
-		const delayTime = mappableDuration.getValue();
-		mappableDuration.next(value);
-		return timer(delayTime).pipe(mapTo(value), first());
+		const duration = isNull(previousDuration) ? initialDuration : mapDuration(previousDuration, value, index);
+		previousDuration = duration;
+		index++;
+		return timer(duration).pipe(mapTo(value), first());
 	};
 };
