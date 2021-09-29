d329767464319fe0ad001f1ca0763480bcc39580
Lucas Paganini <lucaspaganinidm@gmail.com>
2021-09-29 15:10:25 -0300

    Format

--- src/functions/utils/debounce-with-backoff-while.spec.ts
+++ src/functions/utils/debounce-with-backoff-while.spec.ts
@@ -1,3 +1,11 @@
+/*************************************************************************
+ * Copyright 2021 Gravwell, Inc. All rights reserved.
+ * Contact: <legal@gravwell.io>
+ *
+ * This software may be modified and distributed under the terms of the
+ * MIT license. See the LICENSE file for details.
+ **************************************************************************/
+
 import { getTestScheduler, initTestScheduler } from 'jasmine-marbles';
 import { debounceTime } from 'rxjs/operators';
 import { TestScheduler } from 'rxjs/testing';
