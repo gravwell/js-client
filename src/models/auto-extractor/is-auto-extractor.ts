4bed3e412f53dec275b5774a13b02b268d2b7085
puszkarek <guipuszkarek@gmail.com>
2022-06-20 16:13:38 -0300

    ✨ add validators on isAutoExtractor

--- src/models/auto-extractor/is-auto-extractor.ts
+++ src/models/auto-extractor/is-auto-extractor.ts
@@ -6,13 +6,39 @@
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
 
-import { AutoExtractor } from './auto-extractor';
+import { isBoolean, isDate, isNull, isString } from 'lodash';
+import { isNumericID, isUUID } from '~/value-objects';
+import { AutoExtractor, AutoExtractorModule } from './auto-extractor';
 
-export const isAutoExtractor = (value: any): value is AutoExtractor => {
+export const isAutoExtractor = (value: unknown): value is AutoExtractor => {
 	try {
 		const ae = <AutoExtractor>value;
-		return !!ae;
+		return (
+			isUUID(ae.id) &&
+			isNumericID(ae.userID) &&
+			ae.groupIDs.every(isNumericID) &&
+			isString(ae.name) &&
+			isString(ae.description) &&
+			ae.labels.every(isString) &&
+			isBoolean(ae.isGlobal) &&
+			isDate(ae.lastUpdateDate) &&
+			isString(ae.tag) &&
+			isAutoExtractorModule(ae.module) &&
+			isString(ae.parameters) &&
+			(isString(ae.arguments) || isNull(ae.arguments))
+		);
 	} catch {
 		return false;
 	}
 };
+
+const makeIsAutoExtractorModule = () => {
+	const extractorsModules = ['csv', 'fields', 'regex', 'slice', 'json'] as const;
+
+	return (value: unknown): value is AutoExtractorModule => {
+		const m = <AutoExtractorModule>value;
+
+		return isString(m) && extractorsModules.includes(m);
+	};
+};
+export const isAutoExtractorModule = makeIsAutoExtractorModule();
